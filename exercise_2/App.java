1050e3adb55e024b6b7fc58fdbc8722e1b0f4bf5
robitussin <elizerponiojr@gmail.com>
2022-09-02 15:47:16 +0800

    Added exercise 2

---
+++ exercise_2/App.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) {
+
+        // 1. Create your own music playlist by creating a LinkedList object
+
+        // 2. Add 5 new songs to your Playlist using the add() method
+
+        // 3. Add a new song at the front of the Playlist using the addFirst() method
+
+        // 4. Add a new song at the end of the Playlist using the addLast() method
+
+        // 5. Display the song at the front of the Playlist using the combination of
+        // get() method and the println() method
+
+        // 6. Replace the last song using the set() method
+
+        // 7. Remove the first song using the remove() method
+
+        // 8. Display all songs in your Playlist using the println method
+    }
+}
